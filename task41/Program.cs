df33ef11ed541119d0bc27262c0ac6cc5121c2e8
Ekaterina <missathlon08@gmail.com>
2022-09-16 20:43:10 +0300

    add Task 41

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Vvedite kolichestvo chisel M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < M; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Search(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] arr = new int[M];
+FillArray(arr);
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.WriteLine(Search(arr));
